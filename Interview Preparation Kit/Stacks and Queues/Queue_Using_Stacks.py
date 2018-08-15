f18edbf9738fb588ac51e0216fb4364af0c14423
Sheelabhadra Dey <sheelabhadra@gmail.com>
2018-08-15 02:47:34 -0500

    Add queue using 2 stacks

---
+++ Interview Preparation Kit/Stacks and Queues/Queue_Using_Stacks.py
@@ -0,0 +1,24 @@
+# Create a queue using 2 stacks.
+
+## SOLUTION:
+
+class MyQueue(object):
+    def __init__(self):
+        self.first = []
+        self.second = []
+    
+    def peek(self):
+        if not self.second:
+            while self.first:
+                self.second.append(self.first.pop())
+        return self.second[-1]
+        
+    def pop(self):
+        if not self.second:
+            while self.first:
+                self.second.append(self.first.pop())
+        return self.second.pop()
+    
+    def put(self, value):
+        self.first.append(value)
+        
